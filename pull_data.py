1b3aae3c7bddaa445dd1505ccf8022f099d92098
Carlos Castillo Meneses <65776444+carloscastillom@users.noreply.github.com>
2023-02-19 07:24:03 -0500

    Add files via upload

--- pull_data.py
+++ pull_data.py
@@ -1,32 +1,41 @@
-import yfinance as yf
+from yahooquery import Ticker
+import csv
 
 # Create a list of tickers to compare
 tickers = ["MSFT", "AAPL", "ABNB"]
 
-# Create an empty dictionary to store the fundamental data for each ticker
-fundamentals = {}
+# Create an empty list to store the fundamental data for each ticker
+fundamentals = []
 
 # Loop through each ticker and retrieve the fundamental data
 for ticker in tickers:
-    # Retrieve the stock information using the yfinance library
-    stock_info = yf.Ticker(ticker)
-    
+    # Retrieve the stock information using the yahooquery library
+    stock_info = Ticker(ticker)
 
     # Extract the fundamental data
-    # print(stock_info.info)
-    print(stock_info.fast_info['marketCap'])
-    fundamental_data = stock_info.info
+    fundamental_data = stock_info.summary_detail[ticker]
 
-    # Add the fundamental data to the dictionary
-    fundamentals[ticker] = fundamental_data
+    print("=========")
+    print(ticker)
+    print("=========")
+    print(fundamental_data)
 
-# Print the fundamental data for each ticker
-for ticker, data in fundamentals.items():
-    print(f"Fundamental data for {ticker}:")
-    print(f"Market Cap: {data['marketCap']}")
-    print(f"Trailing P/E: {data['trailingPE']}")
-    print(f"Forward P/E: {data['forwardPE']}")
-    print(f"Price/Book: {data['priceToBook']}")
-    print(f"Enterprise Value/Revenue: {data['enterpriseToRevenue']}")
-    print(f"Enterprise Value/EBITDA: {data['enterpriseToEbitda']}")
-    print("------------------------------")
+    # Add the fundamental data to the list
+    fundamentals.append(fundamental_data)
+
+print("all fundamentals")
+print(fundamentals)
+
+# Open a CSV file for writing
+"""with open('fundamental_data.csv', 'w', newline='') as csvfile:
+    # Define the header row
+    fieldnames = ['Ticker', 'Market Cap', 'Trailing P/E', 'Forward P/E', 'Price/Book', 'Enterprise Value/Revenue', 'Enterprise Value/EBITDA']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+    # Write the header row
+    writer.writeheader()
+
+    # Write the fundamental data for each ticker
+    for data in fundamentals:
+        writer.writerow({'Ticker': data['symbol'], 'Market Cap': data['marketCap'], 'Trailing P/E': data['trailingPE'], 'Forward P/E': data['forwardPE'], 'Price/Book': data['priceToBook'], 'Enterprise Value/Revenue': data['enterpriseToRevenue'], 'Enterprise Value/EBITDA': data['enterpriseToEbitda']})
+"""
